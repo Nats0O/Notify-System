2adc24260ed5fa977f38e973e8fbd90c3f16a658
Nats0O <108611807+Nats0O@users.noreply.github.com>
2022-07-03 12:14:23 +0200

    Add files via upload

---
+++ Notify-System/__resource.lua
@@ -0,0 +1,17 @@
+
+fx_version 'cerulean'
+
+games { 'gta5' }
+
+author 'Nats0'
+
+client_script 'client.lua'
+
+ui_page('html/index.html')
+
+files {
+    'html/index.html',
+    'html/ui.css',
+    'html/ui.js',
+    'html/audio.mp3'
+}
---
+++ Notify-System/client.lua
@@ -0,0 +1,13 @@
+RegisterNetEvent('nats0:manda')
+AddEventHandler('nats0:manda', function(message)
+        SendNUIMessage({
+            name = 'addNotification',
+            type = nil,
+            message = message
+        })
+end)
+
+Notifica = function(msg)
+    TriggerEvent('nats0:manda', source, msg)
+end
+
